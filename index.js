37dfd2ce12dc56f412e88361ffd989a0049c0e8d
EL OMARI Soufyane <elomarisoufyane@gmail.com>
2022-11-18 23:01:46 +0100

    Define Number 2

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ class Converter {
     constructor() {}
 
     convert(romanNumber) {
-        return romanNumber == 1 ? "I" : "ERROR"
+        return romanNumber == 1 ? "I" : romanNumber == 2 ? "II" : "ERROR"
     }
 }
